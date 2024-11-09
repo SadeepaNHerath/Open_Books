5d0f058fa7c43302a8adb15b049f151063f21b38
SadeepaNHerath <sadeepahearth@gmail.com>
2024-11-09 13:13:26 +0530

    login + registor pages

---
+++ open-books-ui/src/app/pages/register/register.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RegistrationRequest } from '../../services/models/registration-request';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../services/services/authentication.service';
+
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+
+  registerRequest: RegistrationRequest = { email: '', firstName: '', lastname: '', password: '' };
+  errorMsg: Array<string> = [];
+
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) {
+
+  }
+
+  login() {
+    this.router.navigate(['/login']);
+  }
+
+  register() {
+    this.errorMsg = [];
+    this.authService.register({
+      body: this.registerRequest
+    }).subscribe({
+      next: () => {
+        this.router.navigate(['activate-account']);
+      },
+      error: (err) => {
+        console.log(err);
+        if (err.error.validationErrors) {
+          this.errorMsg = err.error.validationErrors;
+        } else {
+          this.errorMsg.push(err.error.errorMsg);
+        }
+      }
+    })
+  }
+}
