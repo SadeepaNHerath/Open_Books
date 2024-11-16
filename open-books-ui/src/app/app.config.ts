6b00ca7850ead09e1f10fde8dcb9a057affb10a9
SadeepaNHerath <sadeepahearth@gmail.com>
2024-11-16 12:20:22 +0530

    403 forbidden error fixed

--- open-books-ui/src/app/app.config.ts
+++ open-books-ui/src/app/app.config.ts
@@ -1,19 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
-import { HttpTokenInterceptor } from './services/interceptor/http-token.interceptor';
+import { HttpTokenInterceptor } from './services/inteceptor/http-token.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-  provideZoneChangeDetection({ eventCoalescing: true }),
-  provideRouter(routes),
-  provideHttpClient(),
-  {
-    provide: HTTP_INTERCEPTORS,
-    useClass: HttpTokenInterceptor,
-    multi: true
-  }
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpTokenInterceptor,
+      multi: true 
+    },
   ]
 };
